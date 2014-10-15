95e16ffaeb9951329803a9eaad765e59ce103468
Zack Holbrook <zackh1998@gmail.com>
2014-10-15 15:19:06 -0700

    Fixed the variabel things finally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+ before_filter :vars
+
+def vars
+	@city_names = ["Hawaii","London","New York","Seattle","Sydney","Washington"]
+	@city_links = ["hawaii","london","new_york","seattle","sydney","washington"]
+
+	@holiday_names = ["Christmas","Spring Break","Thanksgiving","Valentine's Day"]
+	@holiday_links = ["christmas","spring_break","thanksgiving","valentines"]
+end
+
 end
