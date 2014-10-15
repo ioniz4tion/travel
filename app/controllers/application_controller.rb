0a3013a7508d818a0be7c8c59d5bf868389d0ecf
16ZHOLBROOK - Zachary N Holbrook <16zholbrook@LCHS-A11-MAC-21071.local>
2014-10-15 11:50:59 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def initialize
-	  @city_names = ["Hawaii","London","New York","Seattle","Sydney","Washington"]
-	  @city_links = ["hawaii","london","new_york","seattle","sydney","washington"]
+  #def initialize
+	#  @city_names = ["Hawaii","London","New York","Seattle","Sydney","Washington"]
+	 # @city_links = ["hawaii","london","new_york","seattle","sydney","washington"]
 
-	  @holiday_names = ["Christmas","Spring Break","Thanksgiving","Valentine's Day"]
-	  @holiday_links = ["christmas","spring_break","thanksgiving","valentines"]
-  end
+	 # @holiday_names = ["Christmas","Spring Break","Thanksgiving","Valentine's Day"]
+	 # @holiday_links = ["christmas","spring_break","thanksgiving","valentines"]
+  #end
 end
