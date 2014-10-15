3d09ada3eae9d899ffa6ef26e1e23d30db848d50
16ZHOLBROOK - Zachary N Holbrook <16zholbrook@LCHS-A11-MAC-21071.local>
2014-10-15 11:29:38 -0700

    Removed variables from nav partial and added them to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  @city_names = ["Hawaii","London","New York","Seattle","Sydney","Washington"]
+  @city_links = ["hawaii","london","new_york","seattle","sydney","washington"]
+
+  @holiday_names = ["Christmas","Spring Break","Thanksgiving","Valentine's Day"]
+  @holiday_links = ["christmas","spring_break","thanksgiving","valentines"]
 end
