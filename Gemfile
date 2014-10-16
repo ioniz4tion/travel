e7dd0dd2a910c1288ad3795ddde9b53ca6af2614
Zack Holbrook <zackh1998@gmail.com>
2014-10-15 20:54:36 -0700

    Weather stuff

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.1.6'
 # Use mysql as the database for Active Record
 gem 'mysql2'
 gem 'seed_dump'
+gem 'yahoo_weather', '~> 1.1.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
+	require 'rubygems'
+	require 'yahoo_weather'
+	require 'date'
+
+	before_filter :weather
+
+	def weather
+		@client = YahooWeather::Client.new
+		@weather = @client.fetch(23417489)		
+	end
 
 end
