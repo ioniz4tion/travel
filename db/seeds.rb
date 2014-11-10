e2d354f69d5fb2602144b9e64718b0b1b4a32b6e
Zack Holbrook <zackh1998@gmail.com>
2014-11-09 17:36:11 -0800

    pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 GenPackage.create!([
-  {name: "Hawaii", code: 23417489, image_name: "Test", image_description: "This is a test", image_link: "720x700.gif"},
+  {name: "Hawaii", code: 23417489, image_name: "Hawaii", image_description: "This is Hawaii.", image_link: "hawaii.jpg"},
   {name: "London", code: 44418, image_name: "Test", image_description: "This is a test", image_link: "720x700.gif"},
-  {name: "New York", code: 2459115, image_name: "Test", image_description: "This is a test", image_link: "720x700.gif"},
+  {name: "New York", code: 2459115, image_name: "New York", image_description: "This is New York.", image_link: "NewYork.jpg"},
   {name: "Seattle", code: 2490383, image_name: "Seattle", image_description: "This is a Seattle.", image_link: "seattle.jpg"},
   {name: "Sydney", code: 1105779, image_name: "Test", image_description: "This is not Seattle.", image_link: "720x700.gif"},
   {name: "Washington", code: 2514815, image_name: "Test", image_description: "This is a test", image_link: "720x700.gif"}
