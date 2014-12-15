2f1acd7d12f96b9d57f211c03d558fffea0db2c7
Parker Piedmont <iqover9000@gmail.com>
2014-12-15 15:31:32 -0800

    streamlined flipper script

--- app/assets/javascripts/startup.js
+++ app/assets/javascripts/startup.js
@@ -60,140 +60,86 @@ $(document).ready(function() {
   var $flipper = $('.flipper'),
       $flipperBtn = $('#flipperBtn'),
       $flipperClose = $('#flipperClose'),
-      $front = $('.flipper .front'),
-      $back = $('.flipper .back'),
-      $weatherMore = $('.flipper .weather-more'),
-      slideNum = 0,
-      flipped = false;
-
-  var flipperFlip = function() {
-
-    if (flipped == false) {
-
-      $flipper.css({
-        "-ms-transform"     : "rotateY(180deg)",
-        "-moz-transform"    : "rotateY(180deg)",
-        "-webkit-transform" : "rotateY(180deg)",
-        "-o-transform"      : "rotateY(180deg)",
-        "transform"         : "rotateY(180deg)"
-      }).delay(1000).show(0, function() {
-        slideNum = slider.getCurrentSlide();
-        slider.reloadSlider({
-          startSlide: slideNum,
-          onSlideNext: doThis,
-          onSlidePrev: doThis,
-          onSlideBefore: doThis,
-          onSlideAfter: doThis
-        });
-        slider1.reloadSlider({
-          startSlide: slideNum,
-          width: 360,
-          height: 340,
-          touchEnabled: false,
-          pager: false,
-          controls: false,
-          infiniteloop: true,
-          auto: false,
-          onSlideNext: doThis,
-          onSlidePrev: doThis,
-          onSlideBefore: doThis,
-          onSlideAfter: doThis
-        });
-        slider2.reloadSlider({
-          startSlide: slideNum,
-          width: 340,
-          height: 662,
-          touchEnabled: false,
-          pager: false,
-          controls: false,
-          infiniteloop: true,
-          auto: false,
-          onSlideNext: doThis,
-          onSlidePrev: doThis,
-          onSlideBefore: doThis,
-          onSlideAfter: doThis
-        });
-      });
-
-      $back.css({
-        "z-index": "21"
-      });
-
-      flipped = true;
-
-      console.log("flip");
-
-    };
-
-  };
-
-  var flipperFlipBack = function() {
-
-    if (flipped == true) {
-
-      $flipper.css({
-        "-ms-transform"     : "rotateY(0deg)",
-        "-moz-transform"    : "rotateY(0deg)",
-        "-webkit-transform" : "rotateY(0deg)",
-        "-o-transform"      : "rotateY(0deg)",
-        "transform"         : "rotateY(0deg)"
-      }).delay(1000).show(0, function() {
-        slideNum = slider.getCurrentSlide();
-        slider.reloadSlider({
-          startSlide: slideNum,
-          onSlideNext: doThis,
-          onSlidePrev: doThis,
-          onSlideBefore: doThis,
-          onSlideAfter: doThis
-        });
-        slider1.reloadSlider({
-          startSlide: slideNum,
-          width: 360,
-          height: 340,
-          touchEnabled: false,
-          pager: false,
-          controls: false,
-          infiniteloop: true,
-          auto: false,
-          onSlideNext: doThis,
-          onSlidePrev: doThis,
-          onSlideBefore: doThis,
-          onSlideAfter: doThis
-        });
-        slider2.reloadSlider({
-          startSlide: slideNum,
-          width: 340,
-          height: 662,
-          touchEnabled: false,
-          pager: false,
-          controls: false,
-          infiniteloop: true,
-          auto: false,
-          onSlideNext: doThis,
-          onSlidePrev: doThis,
-          onSlideBefore: doThis,
-          onSlideAfter: doThis
-        });
-      });
-
-      $back.css({
-        "z-index": "-1"
-      });
-
-      flipped = false;
-
-      console.log("flip back");
-
-    };
-
-  };
+      slideNum = 0;
 
   $flipperBtn.click(function() {
-    flipperFlip();
+    $flipper.addClass('flipper-open').delay(1000).show(0, function() {
+        slideNum = slider.getCurrentSlide();
+        slider.reloadSlider({
+          startSlide: slideNum,
+          onSlideNext: doThis,
+          onSlidePrev: doThis,
+          onSlideBefore: doThis,
+          onSlideAfter: doThis
+        });
+        slider1.reloadSlider({
+          startSlide: slideNum,
+          width: 360,
+          height: 340,
+          touchEnabled: false,
+          pager: false,
+          controls: false,
+          infiniteloop: true,
+          auto: false,
+          onSlideNext: doThis,
+          onSlidePrev: doThis,
+          onSlideBefore: doThis,
+          onSlideAfter: doThis
+        });
+        slider2.reloadSlider({
+          startSlide: slideNum,
+          width: 340,
+          height: 662,
+          touchEnabled: false,
+          pager: false,
+          controls: false,
+          infiniteloop: true,
+          auto: false,
+          onSlideNext: doThis,
+          onSlidePrev: doThis,
+          onSlideBefore: doThis,
+          onSlideAfter: doThis
+        });
   });
 
   $flipperClose.click(function() {
-    flipperFlipBack();
+    $flipper.removeClass('flipper-open').delay(1000).show(0, function() {
+        slideNum = slider.getCurrentSlide();
+        slider.reloadSlider({
+          startSlide: slideNum,
+          onSlideNext: doThis,
+          onSlidePrev: doThis,
+          onSlideBefore: doThis,
+          onSlideAfter: doThis
+        });
+        slider1.reloadSlider({
+          startSlide: slideNum,
+          width: 360,
+          height: 340,
+          touchEnabled: false,
+          pager: false,
+          controls: false,
+          infiniteloop: true,
+          auto: false,
+          onSlideNext: doThis,
+          onSlidePrev: doThis,
+          onSlideBefore: doThis,
+          onSlideAfter: doThis
+        });
+        slider2.reloadSlider({
+          startSlide: slideNum,
+          width: 340,
+          height: 662,
+          touchEnabled: false,
+          pager: false,
+          controls: false,
+          infiniteloop: true,
+          auto: false,
+          onSlideNext: doThis,
+          onSlidePrev: doThis,
+          onSlideBefore: doThis,
+          onSlideAfter: doThis
+        });
   });
 
 });	
