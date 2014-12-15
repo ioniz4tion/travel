d41cb0203e21e88b54105f56ba04ea78fb9218b8
Zack Holbrook <zackh1998@gmail.com>
2014-12-14 18:39:45 -0800

    paragraphs

--- app/controllers/gen_packages_controller.rb
+++ app/controllers/gen_packages_controller.rb
@@ -1,16 +1,23 @@
 class GenPackagesController < ApplicationController
   before_action :set_gen_package, only: [:show, :edit, :update, :destroy]
+  before_action :set_paragraph, only: [:show, :edit, :update_paragraph, :destroy_paragraph]
 
   # GET /gen_packages
   # GET /gen_packages.json
   def index
-    @gen_packages = GenPackage.all.order(:name)
     @city = GenPackage.find_by_name(params[:city])
     @slideshow = []
-    GenPackage.all.order(:name).each do |package|
-      @slideshow << {name: package.name, link: package.image_link, description: package.image_description}
-    end
+    @slideshow << {name: @city.name, link: @city.image.url}
 
+    randRecords = @city.gen_attractions.order("RANDOM()")
+    @slideshow << {name: randRecords[0].name, link: randRecords[0].image.url}
+    @slideshow << {name: randRecords[1].name, link: randRecords[1].image.url}
+    @slideshow << {name: randRecords[2].name, link: randRecords[2].image.url}
+    
+    randRecords = @city.gen_restaurants.order("RANDOM()")
+    @slideshow << {name: randRecords[0].name, link: randRecords[0].image.url}
+    @slideshow << {name: randRecords[1].name, link: randRecords[1].image.url}
+    
     begin
       @client = YahooWeather::Client.new
       @weather = @client.fetch(@city.code)
@@ -34,17 +41,40 @@ class GenPackagesController < ApplicationController
 
   # POST /gen_packages
   # POST /gen_packages.json
-  def create
-    @gen_package = GenPackage.new(gen_package_params)
+  def create    
+  end
+
+  def create_paragraph
+    @paragraph = GenPackage.find_by_name(params[:city]).gen_package_paragraphs.build(text: "Default")
 
     respond_to do |format|
-      if @gen_package.save
-        format.html { redirect_to @gen_package, notice: 'Gen package was successfully created.' }
-        format.json { render :show, status: :created, location: @gen_package }
+      if @paragraph.save
+        format.html { redirect_to "/" + params[:city], notice: 'Paragraph was successfully created.' }
+        format.json { render :show, status: :created, location: "/" + params[:city] }
       else
         format.html { render :new }
-        format.json { render json: @gen_package.errors, status: :unprocessable_entity }
+        format.json { render json: @paragraph.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def update_paragraph
+    respond_to do |format|
+      if @paragraph.update(paragraph_params)
+        format.html { redirect_to "/" + params[:city], notice: 'Paragraph was successfully updated.' }
+        format.json { respond_with_bip(@paragraph) }
+      else
+        format.html { render :edit }
+        format.json { render json: @paragraph.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy_paragraph
+    @paragraph.destroy
+    respond_to do |format|
+      format.html { redirect_to "/" + params[:city], notice: 'Paragraph was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
@@ -53,7 +83,7 @@ class GenPackagesController < ApplicationController
   def update
     respond_to do |format|
       if @gen_package.update(gen_package_params)
-        format.html { redirect_to :gen_packages, notice: 'Gen hotel was successfully updated.' }
+        format.html { redirect_to "/" + params[:city], notice: 'Gen hotel was successfully updated.' }
         format.json { respond_with_bip(@gen_package) }
       else
         format.html { render :edit }
@@ -80,6 +110,14 @@ class GenPackagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def gen_package_params
-      params.require(:gen_package).permit(:name, :code, :description, :image_name, :image_link, :image_description)
+      params.require(:gen_package).permit(:image, :name, :code, :description, :image_name, :image_link, :image_description)
+    end
+
+    def set_paragraph
+      @paragraph = GenPackageParagraph.find(params[:id])
+    end
+
+    def paragraph_params
+      params.require(:gen_package_paragraph).permit(:text, :image)
     end
 end
--- app/models/gen_package.rb
+++ app/models/gen_package.rb
@@ -4,4 +4,9 @@ class GenPackage < ActiveRecord::Base
 	has_many :gen_attractions
 	has_many :gen_images
 	has_many :answer_values
+	has_many :gen_package_paragraphs
+
+	has_attached_file :image,	                  
+	                  :default_url => "720x700.gif"
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
