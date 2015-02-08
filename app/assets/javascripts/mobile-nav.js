a9cd59aa63ab0c77287439d3bbb778972c0f0d40
iqover9000 <iqover9000@gmail.com>
2015-02-07 16:32:45 -0800

    finished mobile navbar

--- app/assets/javascripts/mobile-nav.js
+++ app/assets/javascripts/mobile-nav.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+
 	$('nav button').click(function() {
 		$('.nav-arrow').toggleClass('open');
 		$('.nav-mobile').toggleClass('open');
 	});
+
+	$('.package-name').click(function() {
+		$(this).next().toggleClass('open');
+	});
+
 });
