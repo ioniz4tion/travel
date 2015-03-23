7060edd466d30283f38310214979b833568f8f6d
Zack Holbrook <zackh1998@gmail.com>
2015-03-22 17:08:15 -0700

    :aa

--- app/assets/javascripts/fancy-animation.js
+++ app/assets/javascripts/fancy-animation.js
@@ -1,13 +1,18 @@
 
 window.onload = function() {
 	var tl = new TimelineMax();
+	var rotate = new TimelineMax();
 	var w = document.getElementsByClassName('w');
 
-	tl.to(w[0], 0, {x: -75, xPercent: -50})
+	tl.to(w[0], 0, {x: -75, y: -75, xPercent: -50, yPercent: -50})
 	  .to(w[1], 0, {x: -111, xPercent: -50})
 	  .to(w[2], 0, {x: -147, xPercent: -50})
-	  .insert(new TweenMax.to(w[0], 1, {x: 75, yoyo: true, repeat: -1, ease: Sine.easeInOut}))
-	  .insert(new TweenMax.to(w[0], 1, {rotateY: 360, yoyo: true, repeat: -1, ease: Sine.easeInOut}));
+	  .insert(new TweenMax.to(w[0], 1, {x: 75, yoyo: true, repeat: -1, ease: Sine.easeInOut, delay: .5}))
+	  .insert(new TweenMax.to(w[0], 1, {y: 75, yoyo: true, repeat: -1, ease: Sine.easeInOut}));
+
+	//rotate.to(w[0], 1, {y: "+=75", yoyo: true, repeat: -1, ease: Sine.easeInOut})
+
+
 };
 
 
